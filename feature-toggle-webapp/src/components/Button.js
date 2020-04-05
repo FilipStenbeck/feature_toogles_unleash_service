e37defc29521859ec8c6aba7eda03ff6c0dcc7cf
Filip <filip.stenbeck@detectify.com>
2020-04-05 12:53:44 +0200

    make it a bit nicer

--- feature-toggle-webapp/src/components/Button.js
+++ feature-toggle-webapp/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({ children }) => (
-  <div className="button_cont" align="center">
+  <div align="center">
     <a className="cool_button" href="/#">
       {children}
     </a>
